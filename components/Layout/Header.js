ebd702199a49447b565246745438792fd79dd072
Joel Lavaisse <joel.lavaisse@gmail.com>
2022-08-20 04:28:09 -0300

    Add layout component

---
+++ components/Layout/Header.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+
+export default function Header() {
+  return (
+    <Head>
+      <title>Super Useful App</title>
+      <meta name="description" content="This app has a lot of super useful tools" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  )
+}
